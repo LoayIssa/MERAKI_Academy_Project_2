c3904c757dce8baa6d454510ea8a61d8a9314421
LoayIssa <issaloay680@gmail.com>
2021-04-29 12:03:45 +0300

    add if statement to filter in select option

--- page.js
+++ page.js
@@ -1,22 +1,27 @@
 $('.slaider').hide()
 $('.page2').hide()
 $('.ooo').show()
+$('.select').hide()
 
 
  $("#information").on("click", () => {
     $(".page2").show()
     $('.ooo').show()
     $('body').css("background-image","none" )
+    $('.select').show()
+
 
     cardCar()
 }); 
 
-$("#car1").on("click", () => {
-    $(".page2").show()
-    $('.ooo').show()
-    $('body').css("background-image","none" )
+$("select").on("change", () => {
+    if($("#car").val() === "car1"){
+        cardCar1()
+   }
 
-    cardCar1();
+   if($("#car").val() === "car2"){
+    cardCar2()
+}
 }); 
 
 
@@ -47,7 +52,7 @@ const cardCar = () => {
 const carName1= ["chaverolit" , "gms" , "jep"]
 const carDate1 = ["2015" , "2016" ,"2017" , "2010"]
 const carClore1=["red" , "gren" ,"black" ,"red"]
-const image1 = ["./imag/2015-Dodge-Charger-Vancouver-Hellcat.jpg" , "./imag/de0f4bf6e8075312a6eb20ad7044571a.jpg" , "./imag/51859.jpg" ,  ]
+const image1 = ["./imag/2015-Dodge-Charger-Vancouver-Hellcat.jpg"   ]
 
 const cardCar1 = () => {
     $(".page2").show()
@@ -61,11 +66,33 @@ const cardCar1 = () => {
         $(`#infoCar${i}`).append(`<h1>${carName[i]}</h1>`)
         $(`#infoCar${i}`).append(`<p>${carDate[i]}</p>`)
         $(`#infoCar${i}`).append(`<p>${carClore[i]}</p>`)
+        
 
     }
 
 }
+/* ______________ */
 
-function myFunction() {
-    setTimeout(function(){ cardCar1(); }, 3000);
-  }
+const carName2= ["chaverolit" , "gms" , "jep"]
+const carDate2 = ["2015" , "2016" ,"2017" , "2010"]
+const carClore3=["red" , "gren" ,"black" ,"red"]
+const image3 = ["" ,"./imag/2015-Dodge-Charger-Vancouver-Hellcat.jpg"  ]
+
+const cardCar2 = () => {
+    $(".page2").show()
+    $(".page2").html("")
+
+    for(let i=0 ; i<carName2.length ; i++){
+        $('.page2').append(`<div class="ch" id =chcard${i} ></div>`)
+        $(`#chcard${i}`).append(`<img src='${image2[i]}' alt='${image2[i]} photo'>`)
+        
+        $(`#chcard${i}`).append(`<div class=info id='infoCar${i}' ></div>`)
+        $(`#infoCar${i}`).append(`<h1>${carName[i]}</h1>`)
+        $(`#infoCar${i}`).append(`<p>${carDate[i]}</p>`)
+        $(`#infoCar${i}`).append(`<p>${carClore[i]}</p>`)
+        
+
+    }
+
+
+}
